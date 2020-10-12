70afc80c5528d1c4432fd6974f4e44f977a2941e
zukis21 <setyawanzuky@gmail.com>
2020-10-12 14:40:00 +0700

    revisi detail dan home

--- ice_gentong.sql
+++ ice_gentong.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ice_gentong`
+-- Database: `u7590166_ice_gentong`
 --
 
 -- --------------------------------------------------------
