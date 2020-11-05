88871a754b7c50a79fbf0a3c332f1e7b42334b8e
zukis21 <setyawanzuky@gmail.com>
2020-11-05 10:21:11 +0700

    add preloader

--- resources/views/customer/layouts/template-nocart.blade.php
+++ resources/views/customer/layouts/template-nocart.blade.php
@@ -27,9 +27,39 @@
       integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V"
       crossorigin="anonymous"
     />
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <style type="text/css">
+        .preloader{
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            background-color: #fff;
+        }
+
+        .preloader .loading {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%,-50%);
+            font: 14px arial;
+        }
+    </style>
+    <script>
+        $(document).ready(function(){
+          $(".preloader").fadeOut();
+        })
+    </script>
 </head>
 <body>
+    <div class="preloader" id="preloader">
+        <div class="loading">
+          <img src="{{ asset('assets/image/preload.gif') }}" width="80">
+          <p style="font-weight:900;line-height:2;color:#6a3137;margin-left: -10%;">Harap Tunggu</p>
+        </div>
+    </div>
      @if ($message = Session::get('success'))
       <div class="alert alert-success alert-block">
         <button type="button" class="close" data-dismiss="alert">×</button> 
