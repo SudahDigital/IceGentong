af27b906dbfb946c1d410752f4b39fac2e2188bd
zukis21 <setyawanzuky@gmail.com>
2020-11-20 17:01:25 +0700

    change time slide alert

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -427,7 +427,7 @@
         $(".alert").fadeTo(500, 0).slideUp(500, function(){
             $(this).remove(); 
         });
-        }, 2000);
+        }, 3000);
     </script>
     @yield('footer-scripts')
 </body>
