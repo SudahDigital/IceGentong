4c0fa19f18dabe793775f6e0819fd868a43320cb
zukis21 <setyawanzuky@gmail.com>
2020-11-25 16:57:13 +0700

    change/revisi gtag.js analytics

--- resources/views/customer/layouts/template.blade.php
+++ resources/views/customer/layouts/template.blade.php
@@ -29,12 +29,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-183852861-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-183852861-1');
-    </script>
     <style type="text/css">
         .preloader{
             position: fixed;
@@ -587,12 +581,12 @@
             $(this).remove(); 
         });
         }, 4000);
-
-        //analytics
+    </script>
+    <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-Q5EG4YYH5S');
+        gtag('config', 'UA-183852861-1');
     </script>
 </body>
 
