0aa2087f9765af54b24ec5a69fa9878446edac8a
zukis21 <setyawanzuky@gmail.com>
2020-11-25 15:05:28 +0700

    add ga.js analytics

--- resources/views/customer/layouts/template-nobanner.blade.php
+++ resources/views/customer/layouts/template-nobanner.blade.php
@@ -28,13 +28,16 @@
       crossorigin="anonymous"
     />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KLCTDZT');</script>
-        <!-- End Google Tag Manager -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-183852861-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-183852861-1');
+    </script>
+
     <style type="text/css">
         .preloader{
             position: fixed;
@@ -61,10 +64,6 @@
     </script>
 </head>
 <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KLCTDZT"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
     <div class="preloader" id="preloader">
         <div class="loading">
           <img src="{{ asset('assets/image/preload.gif') }}" width="80">
