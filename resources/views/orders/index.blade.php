88e473044641e447cc5a552744b5246e07993b05
zukis21 <setyawanzuky@gmail.com>
2020-12-10 12:04:19 +0700

    create report order export excel

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -29,7 +29,7 @@
 			</ul>
 		</div>
 		<div class="col-md-6">
-			<a href="{{route('orders.export_mapping')}}" target="_blank" class="btn btn-success pull-right">Export Excel</a>
+			<a href="{{route('orders.export_mapping')}}" class="btn btn-success pull-right">Export Excel</a>
 		</div>
 	</div>
 </form>	
